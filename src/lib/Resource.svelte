773a1000afcccadb2891e88cf51f96c940c0e4f3
108charlotte <108sarahsmith@gmail.com>
2025-07-22 18:07:11 -0400

    Added more resources to database

--- src/lib/Resource.svelte
+++ src/lib/Resource.svelte
@@ -4,8 +4,8 @@
 
 <div class="border bg-white rounded-lg p-2 shadow-md m-2">
     <h2 class="font-bold">{title}</h2>
-    <p>{rating}/10</p>
+    <p class="text-green-500">{rating}/10</p>
+    <p class="text-yellow-500">{is_paid}</p>
     <p>{description}</p>
-    <p>{is_paid}</p>
     <a href={link} target="_blank" class="text-blue-500 hover:underline">Check it out</a>
 </div>
